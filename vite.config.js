c98f3384005a1ba045c80d804649b328ef368682
seungho-jg <tmdgh971229@gmail.com>
2024-09-27 18:31:02 +0900

    Fix: Vite options 설정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 // vite.config.js
 export default {
+    optimizeDeps: {
+      include: ['highlight.js', 'highlight.js/lib/languages/javascript', 'highlight.js/lib/languages/python'],
+    },
     base: '/',
   }
   
